6832cd5b1424e9b938f96eddb4a307ba720312e3
unknown <kerch83@gmail.com>
2020-06-19 22:46:01 +0300

    files

---
+++ src/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="">
+    <q-input class="" v-model="input" label="" @change="onChange">
+      <template v-slot:prepend>
+        <q-icon name="input" />
+      </template>
+    </q-input>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'PageIndex',
+      input: ''
+    }
+  },
+  methods: {
+    onChange () {
+      console.log('onChange')
+    }
+  }
+}
+</script>
