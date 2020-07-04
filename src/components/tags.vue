96ac682543298fbb881a4255d605c92bf95770c6
unknown <kerch83@gmail.com>
2020-07-04 22:43:46 +0300

    tags

---
+++ src/components/tags.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="q-sm">
+    <q-chip clickable>
+      tag1
+    </q-chip>
+    <q-chip clickable>
+      tag2
+    </q-chip>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tagsList',
+  data () {
+    return {}
+  }
+}
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -47,9 +47,19 @@
       show-if-above
       bordered
       :width=500>
+      <q-splitter
+        horizontal
+        v-model="splitterMap"
+        style="height:100%">
+        <template v-slot:before>
+      <tagsList/>
+        </template>
+        <template v-slot:after>
       <l-map ref="myMap">
         <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
       </l-map>
+        </template>
+      </q-splitter>
     </q-drawer>
     <q-page-container>
       <router-view />
@@ -58,18 +68,19 @@
 </template>
 
 <script>
-// import EssentialLink from 'components/EssentialLink'
+import tagsList from 'components/tags'
 
 export default {
   name: 'MainLayout',
   inputTag: '',
 
-  //  components: {
-  //    EssentialLink
-  //  },
+  components: {
+    tagsList
+  },
 
   data () {
     return {
+      splitterMap: 10,
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       mapOpen: false,
